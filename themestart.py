a2afc8a1df188f8746d5b570878dea230261d0f4
jorge correa <jcorrea@yaco.es>
2011-07-07 14:11:46 +0200

    with this changes y put the web a little bit fluid but not enought

--- themestart.py
+++ themestart.py
@@ -26,44 +26,55 @@ from merengue.theming.checker import check_themes
 import shutil
 
 
-def create_default_css(themestart, mediadir, theme_name, valuesDic):
+def create_default_css(themestart, mediadir, theme_name, templatedir, valuesDic):
       columns =  valuesDic['leftcolumn']  + valuesDic['rightcolumn']
       maincolumn = valuesDic['portalwidth'] - columns - valuesDic['dmargin']*2 -valuesDic['dpadding']*2
       valuesDic['leftcontent'] = '0px'
       valuesDic['rightcontent'] = '0px'
+      metric =  'px'
+      minrest = 2
+      if (valuesDic['portalwidth']==0):
+          valuesDic['portalwidth'] = '100'
+          maincolumn = 99.35 + maincolumn
+          metric =  '%'
+          minrest = 0
+
       if (valuesDic['leftcolumn'] == 0):
           valuesDic['leftcolumn'] = '0px; display: none; visibility: hidden'
           shutil.copy2(themestart + '/inc.rightsidebar.html', templatedir + '/' + theme_name + '/inc.rightsidebar.html')
       else:
-          valuesDic['leftcontent'] = str(valuesDic['leftcolumn']-valuesDic['dpadding']*2-2) + 'px'
-          valuesDic['leftcolumn']  = str(valuesDic['leftcolumn']) + 'px'
-          maincolumn = maincolumn - valuesDic['dmargin']*2
+          valuesDic['leftcontent'] = valuesDic['leftcolumn']-valuesDic['dpadding']*2 - minrest
+          maincolumn = maincolumn  - valuesDic['dmargin']*2
+          valuesDic['leftcolumn'] =   valuesDic['leftcolumn'] - valuesDic['dmargin']*2
 
       if (valuesDic['rightcolumn'] == 0):
           valuesDic['rightcolumn'] = '0px; display: none; visibility: hidden'
           shutil.copy2(themestart + '/inc.leftsidebar.html', templatedir + '/' + theme_name + '/inc.leftsidebar.html')
       else:
-          valuesDic['rightcontent'] = str(valuesDic['rightcolumn'] - valuesDic['dpadding']*2-2) + 'px'
-          valuesDic['rightcolumn'] = str(valuesDic['rightcolumn']) + 'px'
+          valuesDic['rightcontent'] = valuesDic['rightcolumn'] - valuesDic['dpadding']*2-minrest
           maincolumn = maincolumn - valuesDic['dmargin']*2
-      
-      if (valuesDic['portalwidth']==0):
-          valuesDic['portalwidth'] = '100%'
-      else:
-          valuesDic['portalwidth'] = str(valuesDic['portalwidth']) + 'px'
+          valuesDic['rightcolumn']= valuesDic['rightcolumn'] - valuesDic['dmargin']*2
 
-      valuesDic['maincolumn'] = str(maincolumn) +'px'
+      valuesDic['maincolumn'] = maincolumn
       print 'Creating theme'
       contents = open(themestart + "/default.css", "r").read()
       o = open(mediadir + '/' + theme_name + "/css/default.css", "w")
-      contents = contents.replace("portalwidth", valuesDic['portalwidth'])
-      contents = contents.replace("leftcolumn", valuesDic['leftcolumn'])
-      contents = contents.replace("leftcontent", valuesDic['leftcontent'])
-      contents = contents.replace("rightcolumn", valuesDic['rightcolumn'])
-      contents = contents.replace("rightcontent", valuesDic['rightcontent'])
-      contents = contents.replace("maincolumn", valuesDic['maincolumn'])
-      contents = contents.replace("dpadding", str(valuesDic['dpadding']) + 'px')
-      contents = contents.replace("dmargin", str(valuesDic['dmargin']) + 'px')
+      if (metric=='px'):
+          contents = open(themestart + "/default.css", "r").read()
+      else:
+          contents = open(themestart + "/defaultfluid.css", "r").read()
+      contents = contents.replace("portalwidth", str(valuesDic['portalwidth']) + metric )
+      contents = contents.replace("leftcolumn", str(valuesDic['leftcolumn']) + metric)
+      contents = contents.replace("rightcolumn", str(valuesDic['rightcolumn']) + metric)
+      if (metric=='px'):
+          contents = contents.replace("leftcontent", str(valuesDic['leftcontent']) + metric)
+          contents = contents.replace("rightcontent", str(valuesDic['rightcontent']) + metric)
+      else:
+          contents = contents.replace("leftcontent", 'auto');
+          contents = contents.replace("rightcontent", 'auto');
+      contents = contents.replace("maincolumn", str(valuesDic['maincolumn'])+ metric)
+      contents = contents.replace("dpadding", str(valuesDic['dpadding']) + metric)
+      contents = contents.replace("dmargin", str(valuesDic['dmargin']) + metric)
       contents = contents.replace("color1", valuesDic['color1'])
       contents = contents.replace("color2", valuesDic['color2'])
       contents = contents.replace("color3", valuesDic['color3'])
@@ -95,17 +106,25 @@ class Command(MerengueCommand):
             shutil.copy2(themestart + '/layout.css', mediadir + '/' + theme_name + '/css/layout.css')
             valuesDic = {}
             valuesDic['portalwidth'] = int(input("Select portal width (0 = full screen, or a value for portal width tipical values are between 800 and 1024): "))
-            valuesDic['leftcolumn'] = int(input("Select left column width (0 = widthout left column: "))
-            valuesDic['rightcolumn'] = int(input("Select right column width (0 = widthout right column: "))
-            valuesDic['dpadding'] = int(input("Select default main blocks paddings: "))
-            valuesDic['dmargin'] = int(input("Select default main blocks margin: "))
+            if(valuesDic['portalwidth']==0):
+                valuesDic['leftcolumn'] = int(input("Select left column percentaje width (0 = widthout left column, othercase (12 to 20 %)): "))
+                valuesDic['rightcolumn'] = int(input("Select right column percentaje width (0 = widthout right column, othercase (12 to 20 %)): "))
+                valuesDic['dpadding'] = 2
+                valuesDic['dmargin'] = 2
+                #valuesDic['dpadding'] = int(input("Select default main blocks paddings (between 0 and 3): "))
+                #valuesDic['dmargin'] = int(input("Select default main blocks margin (between 0 and 3): "))
+            else:
+                valuesDic['leftcolumn'] = int(input("Select left column width (0 = widthout left column): "))
+                valuesDic['rightcolumn'] = int(input("Select right column width (0 = widthout right column): "))
+                valuesDic['dpadding'] = int(input("Select default main blocks paddings: "))
+                valuesDic['dmargin'] = int(input("Select default main blocks margin: "))
             valuesDic['color1'] = raw_input("Select the main text color (black, blue, #dedede) :")
             valuesDic['color2'] = raw_input("Select the body background color :")
             valuesDic['color3'] = raw_input("Select link color: ")
             valuesDic['color4'] = raw_input("Select link hover color: ")
 
             
-            create_default_css(themestart, mediadir, theme_name, valuesDic)
+            create_default_css(themestart, mediadir, theme_name, templatedir, valuesDic)
             check_themes() #this line add the new theme to theming table in the project BD
             print 'To start using your new theme activate it in your admin'
             print 'To change your theme design you could edit css in: ' +  os.path.join(mediadir, theme_name) + '/css/'
